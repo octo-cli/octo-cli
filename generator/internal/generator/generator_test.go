c15825c7477798ec3027600f9103aa5a3699248a
Will Roden <willabides@github.com>
2018-10-22 17:00:46 -0500

    remove commented test

--- generator/internal/generator/generator_test.go
+++ generator/internal/generator/generator_test.go
@@ -44,28 +44,6 @@ func Test_svc_writePkg(t *testing.T) {
 	assert.Equal(t, string(want), buf.String())
 }
 
-//func Test_genCliRun_Run(t *testing.T) {
-//	wantIssuessvc, err := ioutil.ReadFile("testdata/exampleapp/services/issuessvc/issuessvc.go")
-//	require.Nil(t, err)
-//	err = os.MkdirAll("./tmp", 0755)
-//	require.Nil(t, err)
-//	tempDir, err := ioutil.TempDir("./tmp", "")
-//	defer func() {
-//		require.Nil(t, os.RemoveAll(tempDir))
-//	}()
-//	require.Nil(t, err)
-//	k := &genCliRun{
-//		RoutesPath: "testdata/routes.json",
-//		OutputPath: tempDir,
-//		ConfigFile: "testdata/exampleapp_config.hcl",
-//	}
-//	err = k.Run()
-//	assert.Nil(t, err)
-//	gotIssuessvc, err := ioutil.ReadFile(filepath.Join(tempDir, "services/issuessvc/issuessvc.go"))
-//	assert.Nil(t, err)
-//	assert.Equal(t, string(wantIssuessvc), string(gotIssuessvc))
-//}
-
 func nsf(t *testing.T, name, ftype string, tag ...*structtag.Tag) *structField {
 	t.Helper()
 	tags, err := newTags(tag...)
