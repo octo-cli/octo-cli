564ecfce44322f201e57b602d1eef9a40c5c49ab
Will Roden <willabides@github.com>
2018-11-04 14:24:57 -0600

    make tests record whenever TESTUSER_TOKEN is set

--- tests/issues_test.go
+++ tests/issues_test.go
@@ -2,14 +2,47 @@ package tests
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/octo-cli/octo-cli/generated"
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
+	"text/tabwriter"
+	"text/template"
 )
 
 func TestIssues(t *testing.T) {
 	t.Run("Create", func(t *testing.T) {
-		stdout, stderr, err := testCmdLine(t, "test_issues_create", &generated.IssuesCmd{},
+		format := `{{.state}} : {{.title}} : {{.body}}`
+		newCmdLine(`issues`,
+			`create`,
+			`--owner=octo-cli-testorg`,
+			`--repo=test-create-issue`,
+			`--title=test this`,
+			`--body=test this body`,
+			`--labels=label1`,
+			`--labels=label2`,
+			`--milestone=1`,
+			`--assignees=octo-cli-testuser`,
+			`--format`, format).
+			test(t, "test_issues_create", "open : test this : test this body\n", "", false)
+
+	})
+
+	t.Run("Get", func(t *testing.T) {
+		format := `{{.state}} : {{.title}} : {{.body}}`
+		newCmdLine(`issues`,
+			`get`,
+			`--owner=octo-cli-testorg`,
+			`--repo=test-create-issue`,
+			`--number=1`,
+			`--format`, format,
+		).test(t, "test_issues_get",  "open : \"test this\" : \"test this body\"\n", "", false)
+	})
+
+	t.Run("play with  output", func(t *testing.T) {
+		// just keeping this here until I put it somewhere else because I don't want to forget how it works.
+		stdout, stderr, err := runCmdLine(t, "test_issues_create", &generated.IssuesCmd{},
 			`create`,
 			`--owner=octo-cli-testorg`,
 			`--repo=test-create-issue`,
@@ -26,9 +59,26 @@ func TestIssues(t *testing.T) {
 		var got map[string]interface{}
 		err = json.Unmarshal(stdout.Bytes(), &got)
 		assert.NoError(t, err)
-		assert.Equal(t, "test this", got["title"])
-		assert.Equal(t, "test this body", got["body"])
-		assert.Len(t, got["labels"], 2)
-		assert.EqualValues(t, 1, got["milestone"].(map[string]interface{})["number"])
+
+		tp, err := template.New("").Funcs(template.FuncMap{"wider": func(a, b interface{}) string { return fmt.Sprintf("%-20s%d", a, b) }}).Parse(`
+	Number	{{.number}}
+	State	{{.state}}
+	Title	{{.title}}
+	User	{{.user.login}}
+	URL	{{.html_url}}
+	CreatedAt	{{.created_at}}
+	UpdatedAt	{{.updated_at}}
+	{{if .labels}}Labels{{range .labels}}	{{.name}}
+	{{end}}{{end}}{{if .milestone}}Milestone	{{.milestone.title}}{{end}}
+	{{if .assignees}}Assignees{{range .assignees}}	{{.login}}
+	{{end}}{{end}}
+	`)
+		assert.NoError(t, err)
+		w := tabwriter.NewWriter(os.Stdout, 8, 8, 8, ' ', 0)
+		err = tp.Execute(w, got)
+		assert.NoError(t, err)
+		err = w.Flush()
+		assert.NoError(t, err)
+
 	})
 }
